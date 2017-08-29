658b82302e0ac1e099ae68553627a59ec7424878
SavvStudio <savvascyp@hotmail.com>
2017-08-29 21:14:43 +0100

    RHARD-0008: relocated config .ini file to root directory

--- spotify_configuration.py
+++ spotify_configuration.py
@@ -2,6 +2,6 @@ import configparser
 
 def get_configuration():
     config = configparser.ConfigParser()
-    config.read("C:\\Users\\Savvas\\RHardstyleSpotifyAccess.ini")
+    config.read("RHardstyleSpotifyAccess.ini")
     access = config["RHARDSTYLESPOTIFY_ACCESS"]
     return access
